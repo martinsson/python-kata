8862a4452740e6ae797c1d6c7ced8677e48d4194
Johan MARTINSSON <martinsson.johan@gmail.com>
2016-09-05 13:43:34 +0200

    made test setup working with pytest

---
+++ src/primes.py
@@ -0,0 +1,18 @@
+'''
+Created on Aug 31, 2016
+
+@author: johan
+'''
+
+class Primes(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        
+        '''
+        
---
+++ test/test_primes.py
@@ -0,0 +1,9 @@
+
+def func(number):
+    return number + 1
+
+def test_returns_empty_list_for_numbers_below_2():
+    assert func(3) == 4
+
+
+

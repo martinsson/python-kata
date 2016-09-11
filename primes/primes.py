c2dd4cb6ba4a59757ab998009a414804575874f3
Johan MARTINSSON <martinsson.johan@gmail.com>
2016-09-12 00:40:24 +0200

    seeing the repetition of 2,3 and 5

--- primes/primes.py
+++ primes/primes.py
@@ -16,13 +16,31 @@ class Primes(object):
         
         '''
     
-    def factorsOf(self, number):
-        if (number < 2):
-            return []
-        elif (number == 4):
-            return [2, 2]
-        else:
-            return [number]
+
+
+    def addOccurencesOfPotentialPrime(self, number, result, potentialPrime):
+        while number % potentialPrime == 0 and number > potentialPrime:
+            result.append(potentialPrime)
+            number = number / potentialPrime
         
+        return number
+
+    def factorsOf(self, number):
+        result = []
+        if (number < 2):
+            return result
+            
+        potentialPrime = 2
+        number = self.addOccurencesOfPotentialPrime(number, result, potentialPrime)
+        
+        potentialPrime = 3
+        number = self.addOccurencesOfPotentialPrime(number, result, potentialPrime)
+        
+        potentialPrime = 5
+        number = self.addOccurencesOfPotentialPrime(number, result, potentialPrime)
+                
+        result.append(number)
+        return result
+    
     
         
--- primes/test_primes.py
+++ primes/test_primes.py
@@ -16,6 +16,12 @@ def test_returns_empty_list_for_numbers_below_2():
 def test_returns_prime_number_wrapped_in_list_for_primes():
     assert factorsOf(2) == [2]
     assert factorsOf(3) == [3]
+    assert factorsOf(7) == [7]
+    assert factorsOf(17) == [17]
 
 def test_factors_are_repeated_as_many_times_as_the_number_is_divisible():
     assert factorsOf(4) == [2, 2]
+    assert factorsOf(8) == [2, 2, 2]
+    assert factorsOf(9) == [3, 3]
+    assert factorsOf(25) == [5, 5]
+    
